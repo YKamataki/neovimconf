60d102dd20a4a27d644b088c867fa4a992a7b883
KAMATAKI Yuya <k212302@cc.k.tsukuba-tech.ac.jp>
2024-08-06 18:17:47 +0900

    Add SKK input
    vim-skk/skkeleton

--- init.lua
+++ init.lua
@@ -1,2 +1,17 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+
+-- My own config files
+
+-- enable skkeleton
+vim.cmd([[
+  call skkeleton#config({
+  \ 'eggLikeNewline': v:true,
+  \ 'globalDictionaries': ['/usr/share/skk/SKK-JISYO.L'],
+  \ 'debug': v:false
+\ })
+]])
+-- keymaps
+vim.keymap.set("i", "<C-j>", "<Plug>(skkeleton-enable)")
+vim.keymap.set("c", "<C-j>", "<Plug>(skkeleton-enable)")
+vim.keymap.set("t", "<C-j>", "<Plug>(skkeleton-enable)")
---
+++ lua/plugins/skk.lua
@@ -0,0 +1,26 @@
+-- SKK Japanese input
+return {
+  -- skkeleton(SKK implimentation)
+  {
+    "vim-skk/skkeleton",
+    dependencies = { "vim-denops/denops.vim" },
+  },
+  -- AZIK kana-kanji converting
+  {
+    "YKamataki/skkeleton-azik",
+    dependencies = { "vim-skk/skkeleton" },
+    config = true,
+  },
+  -- cmp
+  {
+    "rinx/cmp-skkeleton",
+    dependencies = { "vim-skk/skkeleton", "hrsh7th/nvim-cmp" },
+    config = true,
+  },
+  -- indicator
+  {
+    "delphinus/skkeleton_indicator.nvim",
+    dependencies = { "vim-skk/skkeleton" },
+    config = true,
+  },
+}
