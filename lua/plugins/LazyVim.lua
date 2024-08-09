a6bab50f27cea9842e8c84d9b3596e0db30b46d6
KAMATAKI Yuya <k212302@cc.k.tsukuba-tech.ac.jp>
2024-08-09 21:42:37 +0900

    Add cmp sources
    - emoji
    - skkeleton

--- lua/plugins/LazyVim.lua
+++ lua/plugins/LazyVim.lua
@@ -3,4 +3,12 @@ return {
   { "LazyVim/LazyVim", opts = {
     colorscheme = "tokyonight-night",
   } },
+  {
+    "hrsh7th/nvim-cmp",
+    dependencies = { "hrsh7th/cmp-emoji" },
+    opts = function(_, opts)
+      table.insert(opts.sources, { name = "skkeleton" })
+      table.insert(opts.sources, { name = "emoji" })
+    end,
+  },
 }
