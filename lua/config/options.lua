6b4ecddb736e2f8fe03e0bc4e6bbda99baf414a1
KAMATAKI Yuya <k212302@cc.k.tsukuba-tech.ac.jp>
2024-09-24 15:27:18 +0900

    set wrap

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,6 +2,8 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 vim.opt.scrolloff = 9999
+-- wrap lines
+vim.opt.wrap = true
 vim.opt.fileencoding = "utf-8"
 vim.opt.fileencodings = "utf-8,iso-2022-jp,sjis,euc-jp,cp932,utf-16"
 -- vim.opt.clipboard = 'unnamed'
