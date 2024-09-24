5d33e91c9e60554e96a63fe3f6bf3a75cc56173f
KAMATAKI Yuya <k212302@cc.k.tsukuba-tech.ac.jp>
2024-09-24 15:27:31 +0900

    update plugins

--- lua/plugins/LazyVim.lua
+++ lua/plugins/LazyVim.lua
@@ -5,10 +5,8 @@ return {
   } },
   {
     "hrsh7th/nvim-cmp",
-    dependencies = { "hrsh7th/cmp-emoji" },
     opts = function(_, opts)
       table.insert(opts.sources, { name = "skkeleton" })
-      table.insert(opts.sources, { name = "emoji" })
     end,
   },
 }
