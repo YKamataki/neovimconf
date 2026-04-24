b2817440facb3bdec9f5044023a3d65b09894895
kamataki.yuya <kamataki.yuya@ditgroup.jp>
2026-04-24 17:14:12 +0900

    Windows

--- lua/plugins/LazyVim.lua
+++ lua/plugins/LazyVim.lua
@@ -1,12 +1,25 @@
 -- LazyVim Settings
 return {
   { "LazyVim/LazyVim", opts = {
-    colorscheme = "tokyonight-night",
+    colorscheme = "default",
   } },
   {
     "hrsh7th/nvim-cmp",
     opts = function(_, opts)
       table.insert(opts.sources, { name = "skkeleton" })
+    end,
+  },
+  {
+    "nvim-treesitter/nvim-treesitter",
+    dependencies = {
+      "nvim-treesitter/nvim-treesitter-textobjects",
+    },
+    build = ":TSUpdate",
+    install = function()
+      require("nvim-treesitter.install").setup({
+        prefer_git = false,
+        compilers = { "gcc" },
+      })
     end,
   },
 }
